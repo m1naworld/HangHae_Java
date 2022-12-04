9e1800c070fdb58283dc4c81db8c1313a754f077
mina <m1nna@naver.com>
2022-12-05 00:19:32 +0900

    [항해 스프링 숙련 1주차 - myselectshopbeta Refactoring] 객체 중복 생성 코드 정리

--- myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ myselectshop/src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -13,10 +13,15 @@ import java.util.List;
 @RequestMapping("/api")
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController() {
+        this.productService =  new ProductService();
+    }
+
     // 관심 상품 등록하기
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         // 응답 보내기
         return productService.createProduct(requestDto);
     }
@@ -24,7 +29,6 @@ public class ProductController {
     // 관심 상품 조회하기
     @GetMapping("/products")
     public List<ProductResponseDto> getProducts() throws SQLException {
-        ProductService productService = new ProductService();
         // 응답 보내기
         return productService.getProducts();
     }
@@ -32,7 +36,6 @@ public class ProductController {
     // 관심 상품 최저가 등록하기
     @PutMapping("/products/{id}")
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) throws SQLException {
-        ProductService productService = new ProductService();
         // 응답 보내기 (업데이트된 상품 id)
         return productService.updateProduct(id, requestDto);
     }
