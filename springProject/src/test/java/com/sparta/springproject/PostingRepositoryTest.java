abdce2569d34b4cecee8e4806d07123d7a1766f4
mina <m1nna@naver.com>
2022-12-07 17:33:22 +0900

    [항해 스프링 숙련 1주차 과제] refactor: 안쓰는 코드 정리

--- springProject/src/test/java/com/sparta/springproject/PostingRepositoryTest.java
+++ springProject/src/test/java/com/sparta/springproject/PostingRepositoryTest.java
@@ -17,51 +17,51 @@ public class PostingRepositoryTest {
     PostingRepository postingRepository;
 
     // 시간 생성 Test
-    @Test
-    void checkPostPosting(){
-        // 영속성 컨텍스트 -> 모은 쿼리들을 한번에 mock객체, mockito 찾아보기 >> jpa와 별개인 테스트 상황
-        PostingRequestDto myPostDto = new PostingRequestDto("취업", "미나냐", "1234", "취뽀하고싶다!");
+//    @Test
+//    void checkPostPosting(){
+//        // 영속성 컨텍스트 -> 모은 쿼리들을 한번에 mock객체, mockito 찾아보기 >> jpa와 별개인 테스트 상황
+//        PostingRequestDto myPostDto = new PostingRequestDto("취업", "미나냐", "1234", "취뽀하고싶다!");
+//
+//        Posting posing  = new Posting(myPostDto);
+//        System.out.println("DB 저장 전 게시글 생성 시간: " + posing.getCreatedAt());
+//
+//        postingRepository.save(posing);
+//        System.out.println("DB 저장 후 게시글 생성 시간: " + posing.getCreatedAt());
+//
+//        List<Posting> myPostDto1 = postingRepository.findAll();
+//        LocalDateTime saveTime = myPostDto1.get(0).getCreatedAt();
+//        System.out.println("디비에 저장된 게시글 생성 시간: " + saveTime);
+//
+//    }
+//
+//    // 비밀번호 확인
+//    @Test
+//    void checkPassword() throws Throwable {
+//
+//        // given
+//        PostingRequestDto myPostDto = new PostingRequestDto("취업", "미나냐", "1234", "취뽀하고싶다!");
+//        Posting posing  = new Posting(myPostDto);
+//        postingRepository.save(posing);
+//
+//
+//        // when
+//        Long id = 1L;
+//        String password = "1234";
+//
+//
+//        // then
+//        Posting result = (Posting) postingRepository.findById(id).orElseThrow(
+//                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
+//        );
+//
+//        if(!result.getPostPassword().equals(password)){
+//            System.out.println("비밀번호 확인 결과: " + false);
+//        }else{
+//            System.out.println("비밀번호 확인 결과: " + true);
+//        }
+//
 
-        Posting posing  = new Posting(myPostDto);
-        System.out.println("DB 저장 전 게시글 생성 시간: " + posing.getCreatedAt());
 
-        postingRepository.save(posing);
-        System.out.println("DB 저장 후 게시글 생성 시간: " + posing.getCreatedAt());
-
-        List<Posting> myPostDto1 = postingRepository.findAll();
-        LocalDateTime saveTime = myPostDto1.get(0).getCreatedAt();
-        System.out.println("디비에 저장된 게시글 생성 시간: " + saveTime);
-
-    }
-
-    // 비밀번호 확인
-    @Test
-    void checkPassword() throws Throwable {
-
-        // given
-        PostingRequestDto myPostDto = new PostingRequestDto("취업", "미나냐", "1234", "취뽀하고싶다!");
-        Posting posing  = new Posting(myPostDto);
-        postingRepository.save(posing);
-
-
-        // when
-        Long id = 1L;
-        String password = "1234";
-
-
-        // then
-        Posting result = (Posting) postingRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("게시글이 존재하지 않습니다.")
-        );
-
-        if(!result.getPostPassword().equals(password)){
-            System.out.println("비밀번호 확인 결과: " + false);
-        }else{
-            System.out.println("비밀번호 확인 결과: " + true);
-        }
-
-
-
-    }
+//    }
 
 }
