e45228fef3c06d0250d7ea86bf94ea1b6ac9e05b
mina <m1nna@naver.com>
2022-12-07 10:14:23 +0900

    [항해 스프링 1주차 숙련 과제] - refactor: 시간 entity 따로 분리

--- springProject/src/main/java/com/sparta/springproject/entity/Posting.java
+++ springProject/src/main/java/com/sparta/springproject/entity/Posting.java
@@ -1,23 +1,17 @@
 package com.sparta.springproject.entity;
 
 import com.sparta.springproject.dto.PostingRequestDto;
-import com.sparta.springproject.dto.PostingDto;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Entity
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
-public class Posting {
+public class Posting extends Timestamped{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,12 +26,6 @@ public class Posting {
     @Column(nullable = false)
     private String content;
 
-    @CreatedDate //entity가 생성되고 저장될때 시간이 자동 저장
-    private LocalDateTime createdAt;
-
-    @LastModifiedDate
-    private LocalDateTime modifiedAt;
-
     @OneToMany(mappedBy = "posting", fetch = FetchType.LAZY)
     private List<Comment> comments = new ArrayList<>();
 
@@ -45,15 +33,6 @@ public class Posting {
         this.title = postingRequestDto.getTitle();
         this.username = username;
         this.content = postingRequestDto.getContent();
-    }
-    // ze
-    public Posting(PostingDto postingDto) {
-        this.id = postingDto.getId();
-        this.title = postingDto.getTitle();
-        this.username = postingDto.getUsername();
-        this.content = postingDto.getContent();
-        this.createdAt = postingDto.getCreatedAt();
-        this.modifiedAt = postingDto.getModifiedAt();
     }
 
     public void update(String title, String content){
