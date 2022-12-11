864009049a19bc46a3b1ba727a90b57de14d8dab
mina <m1nna@naver.com>
2022-12-12 01:02:51 +0900

    [항해 스프링 심화 1주차] - Spring Security

---
+++ springsecurity/src/main/java/com/sparta/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,80 @@
+package com.sparta.springsecurity.config;
+
+
+import com.sparta.springsecurity.security.CustomAccessDeniedHandler;
+import com.sparta.springsecurity.security.CustomAuthenticationEntryPoint;
+import com.sparta.springsecurity.security.CustomSecurityFilter;
+import com.sparta.springsecurity.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
+public class WebSecurityConfig {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDeniedHandler customAccessDeniedHandler;
+
+    @Bean // 비밀번호 암호화 기능 등록
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // h2-console 사용 및 resources 접근 허용 설정
+        return (web) -> web.ignoring()
+                .requestMatchers(PathRequest.toH2Console())
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // CSRF 설정
+        http.csrf().disable();
+
+        // 위에 설정한 webSecurityCustomizer()와 같은 내용
+        // http.authorizeRequests()
+        //        .antMatchers("/h2-console/**").permitAll()
+        //        .antMatchers("/css/**").permitAll()
+        //        .antMatchers("/js/**").permitAll()
+        //        .antMatchers("/images/**").permitAll()
+        //        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll();
+
+        http.authorizeRequests().antMatchers("/api/user/**").permitAll()
+                .anyRequest().authenticated();
+
+        // Security 제공 fromLogin
+        //  http.formLogin();
+
+        // Custom 로그인 사용
+        http.formLogin().loginPage("/api/user/login-page").permitAll();
+
+        // Custom Filter 등록하기
+        http.addFilterBefore(new CustomSecurityFilter(userDetailsService, passwordEncoder()), UsernamePasswordAuthenticationFilter.class);
+
+        // 접근 제한 페이지 이동 설정
+        http.exceptionHandling().accessDeniedPage("/api/user/forbidden");
+
+        // 401 Error 처리, Authorization 즉, 인증과정에서 실패할 시 처리
+        http.exceptionHandling().authenticationEntryPoint(customAuthenticationEntryPoint);
+
+        // 403 Error 처리, 인증과는 별개로 추가적인 권한이 충족되지 않는 경우
+        http.exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);
+        return http.build();
+    }
+
+}
